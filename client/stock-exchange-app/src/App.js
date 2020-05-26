bb439b0ebc8d6a386a4c73c782bde1a4729b09cc
dev.rayavaram <dev.rayavaram@gmail.com>
2020-05-26 02:07:57 -0500

    fixed routing issue

--- client/stock-exchange-app/src/App.js
+++ client/stock-exchange-app/src/App.js
@@ -2,10 +2,10 @@ import React ,{Component} from 'react';
 import './styles/App.scss';
 import Login,{Signup} from './components/Login';
 import firebase  from './config/fireauth';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route,  Link} from 'react-router-dom';
-import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route,Switch,  Link} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import Stocks from './components/Stocks'
+import Home from './components/Home'
 class  App extends Component{
   constructor(props){
     super();
@@ -30,7 +30,7 @@ class  App extends Component{
     firebase.auth().signOut().then(() =>{
         console.log("Logout called ");
         this.setState({user:null})
-
+        this.setState({state:this.state})
     }).catch(function(error) {
         // An error happened.
       });
@@ -39,56 +39,71 @@ class  App extends Component{
       ReactDOM.render(<Login/>, document.getElementById('root'));
 
     }
-  routeProtectionCheck() {
-    console.log("this.state.user is "+this.state.user);
-    if(this.state.user!==null && this.state.user!==undefined ) {
-
-      return (
-                <div className="App">
-                  <div className="header">
-                      <button className = " ui button" style={{color:'teal'  ,size:'large'}} onClick={this.logout}>
-                      Logout
-                       </button>
-                      <Navbar/>
-                   </div>
-                   <div className="main">
-                   </div>
-                  <div className="footer">
-                      <Footer/>
-                  </div>
-                </div>
-            
-
-
-      )
-    } else {
-      return (
-                <div className="App">
-                <Router>
-                  <nav>
-                      <Link to="/Login" ></Link>
-                      <Link to="/Signup" ></Link>
-                      <Link to="/" ></Link>
-                  </nav>
-                  <Route exact path="/Login" component={Login}>
+    routeProtectionCheck() {
+      if(!this.state.user) {
+        return (
+          <div className="App">
+            <Router>
+              <nav>
+              <Link to="/Login" ></Link>
+              <Link to="/Signup" ></Link>
+              <Link to="/" ></Link>
+              </nav>
+              <Route exact path="/Login" component={Login}>
+              </Route>
+                <Route exact path="/Signup" component={Signup}>
+              </Route>
+              <Route exact path="/" component={Login}>
+              </Route>
+            </Router>
+          </div>
+        )
+      } else {
+        return (
+          <div className="App">
+            <Router>
+            <button className = " ui button" style={{color:'teal'  ,size:'large'}} onClick={this.logout}>
+                Logout
+            </button>
+              <div id="menu">
+                <nav>
+                  <ul>
+                     <li>
+                      <Link to="/Stocks">My Saved List</Link>
+                    </li>
+                    <li>
+                      <Link to="/Home">Search</Link>
+                    </li>
+                    <li>
+                      <Link exact to="/"></Link>
+                    </li>
+ 
+                    </ul>
+                </nav>
+  
+                <Switch>
+                  <Route path="/Home">
+                    <Home />
                   </Route>
-                    <Route exact path="/Signup" component={Signup}>
+                  <Route path="/Stocks">
+                    <Stocks />
                   </Route>
-                  <Route exact path="/" component={Login}>
+                     <Route path="/">
+                    <Home />
                   </Route>
-                </Router>
+                </Switch>
               </div>
-          )
+            </Router>
+          </div>
+        )
+      }
+    }
+    render(){
+      return (
+        <div>
+          { this.routeProtectionCheck() }
+        </div>
+      )
     }
   }
-  render(){
-    return (
-      <div>
-        { this.routeProtectionCheck() }
-      </div>
-    )
-  }
-
-}
-
-export default App;
+  export default App;
